2d2bbc2ff09ddd79d208d0a8a1d11f713f5cf141
Colgan, Chris <ccolgan@columbia.com>
2016-11-03 16:55:25 -0700

    Standardized some white space for readability

--- PSThousandEyes/Public/Test-TEConfig.ps1
+++ PSThousandEyes/Public/Test-TEConfig.ps1
@@ -1,11 +1,15 @@
 Function Test-TEConfig {
     <#
+
     .SYNOPSIS
     Used to check your configuration. 
+
     .DESCRIPTION
     Used to check your configuration. Calls an API to test the token. 
+
     .EXAMPLE
     Test-TEConfig
+
     #>
 
     $Result = Invoke-TeRequest -Command 'agents'
